4da772b3104c2f17170f7b80c359c702fd0d5465
Service.in <ggiridhar5519@gmail.com>
2026-05-24 10:43:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,0.08)";
+    }
+    else{
+        navbar.style.boxShadow = "none";
+    }
+
+});
