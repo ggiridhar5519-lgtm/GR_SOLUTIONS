27f73993bbca66f57e880732668c828ecee487b3
Service.in <ggiridhar5519@gmail.com>
2026-05-24 11:19:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll",()=>{
 
-    const navbar = document.querySelector(".navbar");
+    const navbar=document.querySelector(".navbar");
 
-    if(window.scrollY > 50){
+    if(window.scrollY>50){
 
-        navbar.style.background = "rgba(2,6,23,0.75)";
-        navbar.style.boxShadow = "0 10px 40px rgba(0,0,0,0.4)";
+        navbar.style.background="rgba(2,6,23,0.75)";
+        navbar.style.boxShadow="0 10px 40px rgba(0,0,0,0.35)";
 
     }
+
     else{
 
-        navbar.style.background = "rgba(255,255,255,0.03)";
-        navbar.style.boxShadow = "none";
+        navbar.style.background="rgba(255,255,255,0.03)";
+        navbar.style.boxShadow="none";
 
     }
 
