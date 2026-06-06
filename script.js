41e48da17547e62489797300f9613194690bef2e
GR_SOLUTIONS <ggiridhar5519@gmail.com>
2026-06-06 10:41:44 +0530

    Refactor scroll event listener for navbar styling

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 window.addEventListener("scroll",()=>{
 
-    const navbar=document.querySelector(".navbar");
+const navbar=document.querySelector(".navbar");
 
-    if(window.scrollY>50){
+if(window.scrollY>50){
 
-        navbar.style.background="rgba(2,6,23,0.75)";
-        navbar.style.boxShadow="0 10px 40px rgba(0,0,0,0.35)";
+navbar.style.background="rgba(2,6,23,0.75)";
+navbar.style.boxShadow="0 10px 40px rgba(0,0,0,0.35)";
 
-    }
+}else{
 
-    else{
+navbar.style.background="rgba(255,255,255,0.03)";
+navbar.style.boxShadow="none";
 
-        navbar.style.background="rgba(255,255,255,0.03)";
-        navbar.style.boxShadow="none";
-
-    }
+}
 
 });
